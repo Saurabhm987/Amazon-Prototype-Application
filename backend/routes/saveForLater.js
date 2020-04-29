d7820664badc1c28a8e644acf2b0377a02ec4a23
sreejasriram <sreeja.sriram@sjsu.edu>
2020-04-28 20:57:26 -0700

    save for later API

--- backend/routes/saveForLater.js
+++ backend/routes/saveForLater.js
@@ -3,17 +3,17 @@ const express = require('express')
 const router = express.Router()
 var mongoose = require('mongoose');
 // const buyer = require('../dbModels/buyer')
-const cartServices = require('../services/cart')
+const saveForLaterServices = require('../services/saveForLater')
 
 
-router.get('/getCart/:id', async (request, response) => {  
+router.get('/getSaveForLater/:id', async (request, response) => {  
     try {
         const data = {
             "body": request.body,
             "params": request.params,
             "query": request.query,
         }
-        let res =await cartServices.getProductsFromCart(data);
+        let res =await saveForLaterServices.getProductsFromSaveForLater(data);
         response.status(res.status).json(res.body);     
     }  
     catch (error) {
@@ -30,7 +30,7 @@ router.get('/getCart/:id', async (request, response) => {
         return response.status(code).json({ message });
     }
 });
-router.post('/addToCart/:customer_id', async (request, response) => {  
+router.post('/addToSaveForLater/:customer_id', async (request, response) => {  
     try {
         const data = {
             "body": request.body,
@@ -38,7 +38,7 @@ router.post('/addToCart/:customer_id', async (request, response) => {
             "query": request.query,
         }
         console.log(data)
-        let res =await cartServices.addProductInCart(data);
+        let res =await saveForLaterServices.addProductInSaveForLater(data);
         response.status(res.status).json(res.body);
     }  
     catch (error) {
@@ -55,43 +55,19 @@ router.post('/addToCart/:customer_id', async (request, response) => {
         return response.status(code).json({ message });
     }
 });
-router.put('/updateCart/:customer_id/product/:product_id', async (request, response) => {  
+
+
+
+router.delete('/deleteSaveForLater/:customer_id/product/:product_id/:type', async (request, response) => {  
     try {
         const data = {
             "body": request.body,
             "params": request.params,
             "query": request.query,
+            "type": "deleteProductInSaveForLater"
         }
-        let res =await cartServices.updateProductInCart(data);
-        response.status(res.status).json(res.body);
-    }     
-    catch (error) {
-        if (error.message)
-            message = error.message
-        else
-            message = 'Error while updating cart'
-        
-        if (error.statusCode)
-            code = error.statusCode
-        else
-            code = 500
-
-        return response.status(code).json({ message });
-    }
-});
-
-
-
-router.delete('/deleteCart/:customer_id/product/:product_id/:type', async (request, response) => {  
-    try {
-        const data = {
-            "body": request.body,
-            "params": request.params,
-            "query": request.query,
-            "type": "deleteProductInCart"
-        }
-       
-        let res =await cartServices.deleteProductInCart(data);
+      
+        let res =await saveForLaterServices.deleteProductInSaveForLater(data);
         response.status(res.status).json(res.body);
     } catch (error) {
         if (error.message)
