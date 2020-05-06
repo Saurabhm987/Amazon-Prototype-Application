724ac0e7c3c2b275b9f7cb4ad3d2da5892628f48
Shrey Patel <shrey2411@gmail.com>
2020-05-05 23:37:21 -0700

    page redirect bugfix

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -6,6 +6,7 @@ import ProductCategoryDropdown from '../product/productCategoryDropdown'
 import { Redirect } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { fetchProduct, productCategories } from '../../actions/product';
+import { USER_CUSTOMER, USER_SELLER, USER_ADMIN } from '../controller/config';
 
 import {
   Container,
@@ -39,7 +40,15 @@ class AppHeader extends Component {
     let token = localStorage.getItem('token')
 
     if(token !== null){
-      this.props.history.push('/dashboard')
+      let user = JwtDecode(token);
+      if (user.userType === USER_CUSTOMER) {
+        this.props.history.push('/dashboard');
+      } else if (user.userType === USER_SELLER) {
+        this.props.history.push('/sellerCentral');
+      } else {
+        // his.props.history.push('/sellerCentral');
+      }
+       
     }
 
     await this.props.productCategories()
